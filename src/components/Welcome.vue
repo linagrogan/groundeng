2c0b60a6f8c4c8cbaeb9d348b5d761ade51ade6c
Alina Yakovleva <linagrogan@gmail.com>
2018-12-02 16:14:14 +0300

    Remove image from Welcome component

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,5 @@
 <template>
   <b-jumbotron header="GroundEng!">
-    <b-img rounded src="https://picsum.photos/1050/200/?image=4" fluid-grow alt="Responsive image"/>
     <p class="mt-3">
       A place where you can learn English in a weird way! Like SkyEng, but far worse!
       In order to play, you have got to have an account. Please login or register.
