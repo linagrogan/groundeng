48addac4b2767d9a623f756071badc00c338f9e8
Alina Yakovleva <linagrogan@gmail.com>
2018-12-02 16:12:55 +0300

    Add navigation to MainMenu component

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -3,12 +3,12 @@
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand href="#">GroundEng</b-navbar-brand>
+    <b-navbar-brand>GroundEng</b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
 
       <b-navbar-nav>
-        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item to="/">Home</b-nav-item>
         <b-nav-item href="#" disabled>Play</b-nav-item>
       </b-navbar-nav>
 
